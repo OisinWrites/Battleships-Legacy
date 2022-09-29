146d5967537e46136e3bc76809a3cc42decbd84f
OisinWrites <oisin.banville@gmail.com>
2022-09-29 09:54:02 +0000

    create the ship class that will give its inheritancy to the ship subclasses

--- classes/ship.py
+++ classes/ship.py
@@ -4,3 +4,23 @@ class Ship:
     which used by the subclasses to initialise
     a ship object.
     """
+    def __init__(self, start_coordinate, direction, coordinates):
+        """
+        Each ship on intialisation of its instance must establish its
+        starting point: the primary coordinates,
+        direction: horizontal or vertical to the board
+        coordinates: the list of coordinates it occupies, including the
+        start coordinate, determined by the subclasses' length object
+        and relevant function.
+        Each ship will essentially be this set of coordinates.
+
+        EAch instance must then handle how many of its coordinates
+        have been targeted with valid hits by the opponent, and
+        check whether it is sunk where the damaged tile set equals
+        the coordinates set.
+        """
+        self.start_coordinate = start_coordinate
+        self.direction = direction
+        self.coordinates = coordinates
+        self.number_of_damaged_tiles = 0
+        self.is_sunk = False
