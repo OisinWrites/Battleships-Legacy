852136729c7316d533338f352fcc76033af6e8ff
OisinWrites <oisin.banville@gmail.com>
2022-09-29 14:59:46 +0000

    add welcome screen ASCII to Game class

--- classes/game.py
+++ classes/game.py
@@ -13,3 +13,30 @@ board class, which handles the varied ship subclasses,
 wherein the overarching ship class is nested at the
 very bottom of the chain.
 """
+from .player import Player
+
+
+class Game:
+    """
+    Calls in all other class, objects, and methods, and
+    assembles into one chain of logic.
+    """
+
+    def __init__(self):
+        self.welcome_screen()
+
+    def welcome_screen(self):
+        """
+        Function displays welcome screen with ASCII art
+        """
+        print("""______       __   __   __
+                |   __ .---.-|  |_|  |_|  .-----.
+                |   __ |  _  |   _|   _|  |  -__|
+                |______|___._|____|____|__|_____|
+                           __    __
+                    .-----|  |--|__.-----.-----.
+                    |__ --|     |  |  _  |__ --|
+                    |_____|__|__|__|   __|_____|
+                                   |__|
+        """
+              )
