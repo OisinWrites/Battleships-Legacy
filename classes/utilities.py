9ebf6b06f07cf69b2e1cd9751c291588387cdd03
OisinWrites <oisin.banville@gmail.com>
2022-09-29 15:33:05 +0000

    def to validate name input and handle empty and computer inputs, with flavour

--- classes/utilities.py
+++ classes/utilities.py
@@ -23,8 +23,26 @@ def size_input():
             print("Surely we haven't run aground?")
             continue
 
+
 def name_input():
     # Take and validate name
+    valid_name = False
+    while not valid_name:
+        name = input("To whom do we report, sir?")
+        if len(name.strip(" ")) == 0:
+            print("Apologies sir, the missiles have left me half deaf."
+                  "Could you repeat that, please?")
+            continue
+
+        elif name.lower() == "computer":
+            print("But sir, that would make you the enemy!?")
+            continue
+
+        name = string.capwords(name)
+        print(f"Welcome Captain {name}! We await your orders")
+        return name
+
+
 def duplicate_tile_check():
     # Checks if tile is already occupied
 def coord_input_validator():
